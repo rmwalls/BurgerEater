2b4a3a60b0cbe91733fbdf503c629b6e71d3e6a5
R Walls <48925240+rmwalls@users.noreply.github.com>
2019-07-22 14:37:08 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured) VALUES 
-("black bean", FALSE),
-("veggie", FALSE),
-("cheese", FALSE)
+INSERT INTO burgers (burger_name) VALUES 
+("black bean"),
+("veggie"),
+("cheese")
